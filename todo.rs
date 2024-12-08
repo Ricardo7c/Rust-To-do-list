bedb014e6da894f38f64e79e1485fb13e28762e0
Ricardo7c <Ricardo7c@hotmail.com>
2024-12-08 16:24:38 -0300

    Finalizado!

--- todo.rs
+++ todo.rs
@@ -6,7 +6,7 @@ fn input(texto:&str) -> String{
     io::stdout().flush().unwrap();
     let mut x = String::new();
     io::stdin().read_line(&mut x).unwrap();
-    x
+    x.trim().to_string()
 
 }
 
@@ -18,26 +18,47 @@ fn add(nome: String, lista: &mut Vec<Tarefa> ){
     lista.push(Tarefa{nome});
 }
 
+fn limpar_tela() {
+    if cfg!(target_os = "windows") {
+        Command::new("cmd").args(&["/C", "cls"]).status().unwrap();
+    } else {
+        Command::new("clear").status().unwrap();
+    }
+}
+
 
 fn main(){
     let mut lista :Vec<Tarefa> = vec![];
     loop{
-        Command::new("cmd")
-        .args(&["/C", "cls"])
-        .status()
-        .unwrap();
-
-
+        limpar_tela();
         if !lista.is_empty(){
             println!("===== Lista de Tarefas =======");
             for (id, task) in lista.iter().enumerate() {
-                print!("{} - {}", id, task.nome);
+                println!("{} - {}", id, task.nome);
             }
         }else{
             println!("A lista ainda está vazia!");
         }
         let nome = input("Digite a tarefa que deseja adicionar: ");
-        add(nome, &mut lista);
+        if nome.as_str() == "00" || nome.is_empty(){
+            loop {
+                println!("==== Menu de opções ====");
+                println!("A) Adicionar tarefa: ");
+                println!("B) Remover tarefa:");
+                let opcao = input("Digite a opção desejada: ");
+                if opcao == "A".to_owned(){
+                    break;
+                }else if opcao == "B".to_owned() {
+                    let indice = input("Digite o numero da tarefa que deseja remover: ").parse::<usize>().unwrap();
+                    lista.remove(indice);
+                    break;
+                }else{
+                    println!("Opção invalida, escolha A ou B");
+                }
+            }
+        }else{
+            add(nome, &mut lista);
+        }
     }
 
 }
